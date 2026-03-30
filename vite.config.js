9ceb2b9e8bb3f530253abace2441afbd89e1a42c
fakhrizafauzi <fakhrizafauzii@gmail.com>
2026-03-30 10:49:24 +0700

    new route

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/zen-gallery/',
+  base: '/gallery-zen/',
   plugins: [
     react(),
   ],
